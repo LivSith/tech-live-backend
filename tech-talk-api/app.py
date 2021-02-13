ac2bd98b053c02c81f83fb27aa1cb331ee6e6e49
LivSith <liviasithmendes@gmail.com>
2021-02-13 16:43:28 -0300

    Adicionado detalhe de artigo na URI e tratamento de erro, caso nao tenha artigo.

--- tech-talk-api/app.py
+++ tech-talk-api/app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, \
+    make_response, request
 
 app = Flask(__name__)
 
@@ -21,6 +22,20 @@ articles = [
 @app.route('/techtalk/api/v1.0/articles', methods=['GET'])
 def get_articles():
     return jsonify({'articles': articles})
+
+
+@app.route('/techtalk/api/v1.0/articles/<int:article_id>', methods=['GET'])
+def get_article(article_id):
+    article = [article for article in articles if article['id'] == article_id]
+    if len(article) == 0:
+        abort(404)
+    return jsonify({'article': article[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
